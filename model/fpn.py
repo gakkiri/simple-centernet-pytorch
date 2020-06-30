9a65007070d023db2c13ae4d0f71f7215a721d4c
gakkii <35442235+gakkiri@users.noreply.github.com>
2020-06-30 16:02:05 +0800

    Update fpn.py

--- model/fpn.py
+++ model/fpn.py
@@ -34,7 +34,7 @@ class FPN(nn.Module):
         super(FPN, self).__init__()
 
         self.laterals = nn.Sequential(*[Conv1x1(inplanes // (2 ** c), outplanes) for c in reversed(range(4))])
-        self.smooths = nn.Sequential(*[Conv3x3(outplanes * c, outplanes * c) for c in range(1, 5)])
+        self.smooth = nn.Sequential(*[Conv3x3(outplanes * c, outplanes * c) for c in range(1, 5)])
         self.pooling = nn.MaxPool2d(2)
 
     def forward(self, features):
